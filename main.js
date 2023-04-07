d57b316cf831d2e966394cb4ba14aa6eedba0d07
2009Parth <111286031+2009Parth@users.noreply.github.com>
2023-04-07 21:01:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload()
+{
+
+}
+
+
+
+
+function updateCanvas()
+{
+    canvas.background("white");
+    
+}
+
+
+
+
+
+function setup()
+{
+    canvas = createCanvas(250, 250);
+    canvas.center();
+    background("white");
+}
